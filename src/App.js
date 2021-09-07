25196245bcbe4304df21c47042dc0e9e9ffa8154
kamaleshkc <kamaleshkc999@gmail.com>
2021-09-07 13:34:35 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,564 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { FaBeer } from 'react-icons/fa';
+import Img from './img/chau.jpg'
+import MoMoImg from './img/dau.jpg'
+import FoodImg from './img/momo.jpg'
+import Image from './img/Image.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <div className="app-bar">
+            <div className="flex-item-left">
+                Discount & Order
+            </div>
+            <div className="flex-item-right-last">
+                <div className="app-bar-right">
+                    <div>
+                        <div className="input_container">
+                            <input type="text" placeholder="search" className="input" />
+                            <img src={Image} className="input_img" alt="logo"/>
+                        </div>    
+                    </div>
+                        
+                
+                    <div className="btn-group">
+                        <button className="app-bar-btn"> <FaBeer style={{marginRight:"5px"}}/>Filter</button>
+                        <button className="app-bar-btn"><FaBeer style={{marginRight:"5px"}}/>Short by</button>
+                        <button className="app-bar-btn"><FaBeer style={{marginRight:"5px"}}/>Short by</button>
+                        <button className="app-bar-btn"><FaBeer style={{marginRight:"5px"}}/>Short by</button>
+                    </div>
+
+                </div>
+                
+                
+
+            </div>
+        
+        </div>
+
+        <hr class="rounded"/>
+
+
+    <div className="menu-container">
+        <div className="active">All</div>
+        <div>Active</div>
+        <div>Scheduled</div>
+        <div>Expired</div>
     </div>
+<div className="content-container">
+    <div className="card-container">
+        <div className="card-items">
+            <div className="sub-items">
+                <div className="flex-item-left">
+                        Discount on items(Normal Discount)
+                </div>
+                <div className="flex-item-right-las">
+                        :
+                </div>
+
+
+                
+            </div>
+                
+            <button className="blue-btn" >Active</button>
+            <hr class="rounded"/>
+            <div className="sub-items">
+                <div className="flex-item-left">
+                      Valid Through
+                      <div className="sub-items-small">
+                      <div className="flex-item-left">
+
+                      17/07/2021-18/07/2021
+                      </div>
+                </div>
+                </div>
+                <div className="flex-item-right">
+                        Valid on
+                        <div className="sub-items-small">
+                         <div className="flex-item-left">
+
+                            All Week
+                        </div>
+                      </div>
+                </div>
+
+
+                
+            </div>
+
+            <div className="sub-items">
+                <div className="flex-item-left">
+                        Type
+                      <div className="sub-items-small">
+                      <div className="flex-item-left">
+
+                      17/07/2021-18/07/2021
+                      </div>
+                </div>
+                </div>
+                <div className="flex-item-right">
+                         <div className="image-stack">
+                        <div className="image-a">
+                            <img src={Img}alt="foodImage"a></img>
+                        </div>
+                        <div className="image-a" style={{left:"15px"}}>
+                            <img src={MoMoImg} alt="foodImage"></img>
+                        </div>
+                        <div className="image-a"  style={{left:"30px"}}>
+                            <img src={FoodImg} alt="foodImage"></img>
+                        </div>
+                        
+                        
+                    </div>
+                </div>                
+            </div>
+              
+                
+
+                
+
+                
+
+
+
+
+        </div>
+
+
+    </div>
+
+
+
+    <div className="card-container">
+        <div className="card-items">
+            <div className="sub-items">
+                <div className="flex-item-left">
+                        Discount on items(Normal Discount)
+                </div>
+                <div className="flex-item-right-las">
+                        :
+                </div>
+
+
+                
+            </div>
+                
+            <button className="red-btn" >Expire</button>
+            <hr class="rounded"/>
+            <div className="sub-items">
+                <div className="flex-item-left">
+                      Valid Through
+                      <div className="sub-items-small">
+                      <div className="flex-item-left">
+
+                      17/07/2021-18/07/2021
+                      </div>
+                </div>
+                </div>
+                <div className="flex-item-right">
+                        Valid on
+                        <div className="sub-items-small">
+                         <div className="flex-item-left">
+
+                            All Week
+                        </div>
+                      </div>
+                </div>
+
+
+                
+            </div>
+
+            <div className="sub-items">
+                <div className="flex-item-left">
+                        Type
+                      <div className="sub-items-small">
+                      <div className="flex-item-left">
+
+                      17/07/2021-18/07/2021
+                      </div>
+                </div>
+                </div>
+                <div className="flex-item-right">
+                         <div className="image-stack">
+                        <div className="image-a">
+                            <img src={Img}alt="foodImage"></img>
+                        </div>
+                        <div className="image-a" style={{left:"15px"}}>
+                            <img src={MoMoImg}alt="foodImage"></img>
+                        </div>
+                        <div className="image-a"  style={{left:"30px"}}>
+                            <img src={FoodImg} alt="foodImage"></img>
+                        </div>
+                        
+                        
+                    </div>
+                </div>                
+            </div>
+              
+                
+
+                
+
+                
+
+
+
+
+        </div>
+
+
+    </div>
+
+
+    <div className="card-container">
+        <div className="card-items">
+            <div className="sub-items">
+                <div className="flex-item-left">
+                        Discount on items(Normal Discount)
+                </div>
+                <div className="flex-item-right-las">
+                        :
+                </div>
+
+
+                
+            </div>
+                
+            <button className="green-btn" >Scheduled</button>
+            <hr class="rounded"/>
+            <div className="sub-items">
+                <div className="flex-item-left">
+                      Valid Through
+                      <div className="sub-items-small">
+                      <div className="flex-item-left">
+
+                      17/07/2021-18/07/2021
+                      </div>
+                </div>
+                </div>
+                <div className="flex-item-right">
+                        Valid on
+                        <div className="sub-items-small">
+                         <div className="flex-item-left">
+
+                            All Week
+                        </div>
+                      </div>
+                </div>
+
+
+                
+            </div>
+
+            <div className="sub-items">
+                <div className="flex-item-left">
+                        Type
+                      <div className="sub-items-small">
+                      <div className="flex-item-left">
+
+                      17/07/2021-18/07/2021
+                      </div>
+                </div>
+                </div>
+                <div className="flex-item-right">
+                         <div className="image-stack">
+                        <div className="image-a">
+                            <img src={Img}alt="foodImage"></img>
+                        </div>
+                        <div className="image-a" style={{left:"15px"}}>
+                            <img src={MoMoImg}alt="foodImage"></img>
+                        </div>
+                        <div className="image-a"  style={{left:"30px"}}>
+                            <img src={FoodImg}alt="foodImage" ></img>
+                        </div>
+                        
+                        
+                    </div>
+                </div>                
+            </div>
+              
+                
+            </div>
+                
+
+                
+
+
+
+
+              </div>
+        
+
+        
+
+
+         </div>
+
+
+
+
+         <div className="content-container">
+    <div className="card-container">
+        <div className="card-items">
+            <div className="sub-items">
+                <div className="flex-item-left">
+                        Discount on items(Normal Discount)
+                </div>
+                <div className="flex-item-right-las">
+                        :
+                </div>
+
+
+                
+            </div>
+                
+            <button className="blue-btn" >Active</button>
+            <hr class="rounded"/>
+            <div className="sub-items">
+                <div className="flex-item-left">
+                      Valid Through
+                      <div className="sub-items-small">
+                      <div className="flex-item-left">
+
+                      17/07/2021-18/07/2021
+                      </div>
+                </div>
+                </div>
+                <div className="flex-item-right">
+                        Valid on
+                        <div className="sub-items-small">
+                         <div className="flex-item-left">
+
+                            All Week
+                        </div>
+                      </div>
+                </div>
+
+
+                
+            </div>
+
+            <div className="sub-items">
+                <div className="flex-item-left">
+                        Type
+                      <div className="sub-items-small">
+                      <div className="flex-item-left">
+
+                      17/07/2021-18/07/2021
+                      </div>
+                </div>
+                </div>
+                <div className="flex-item-right">
+                         <div className="image-stack">
+                        <div className="image-a">
+                            <img src={Img}alt="foodImage"a></img>
+                        </div>
+                        <div className="image-a" style={{left:"15px"}}>
+                            <img src={MoMoImg} alt="foodImage"></img>
+                        </div>
+                        <div className="image-a"  style={{left:"30px"}}>
+                            <img src={FoodImg} alt="foodImage"></img>
+                        </div>
+                        
+                        
+                    </div>
+                </div>                
+            </div>
+              
+                
+
+                
+
+                
+
+
+
+
+        </div>
+
+
+    </div>
+
+
+
+    <div className="card-container">
+        <div className="card-items">
+            <div className="sub-items">
+                <div className="flex-item-left">
+                        Discount on items(Normal Discount)
+                </div>
+                <div className="flex-item-right-las">
+                        :
+                </div>
+
+
+                
+            </div>
+                
+            <button className="red-btn" >Expire</button>
+            <hr class="rounded"/>
+            <div className="sub-items">
+                <div className="flex-item-left">
+                      Valid Through
+                      <div className="sub-items-small">
+                      <div className="flex-item-left">
+
+                      17/07/2021-18/07/2021
+                      </div>
+                </div>
+                </div>
+                <div className="flex-item-right">
+                        Valid on
+                        <div className="sub-items-small">
+                         <div className="flex-item-left">
+
+                            All Week
+                        </div>
+                      </div>
+                </div>
+
+
+                
+            </div>
+
+            <div className="sub-items">
+                <div className="flex-item-left">
+                        Type
+                      <div className="sub-items-small">
+                      <div className="flex-item-left">
+
+                      17/07/2021-18/07/2021
+                      </div>
+                </div>
+                </div>
+                <div className="flex-item-right">
+                         <div className="image-stack">
+                        <div className="image-a">
+                            <img src={Img}alt="foodImage"></img>
+                        </div>
+                        <div className="image-a" style={{left:"15px"}}>
+                            <img src={MoMoImg}alt="foodImage"></img>
+                        </div>
+                        <div className="image-a"  style={{left:"30px"}}>
+                            <img src={FoodImg} alt="foodImage"></img>
+                        </div>
+                        
+                        
+                    </div>
+                </div>                
+            </div>
+              
+                
+
+                
+
+                
+
+
+
+
+        </div>
+
+
+    </div>
+
+
+    <div className="card-container">
+        <div className="card-items">
+            <div className="sub-items">
+                <div className="flex-item-left">
+                        Discount on items(Normal Discount)
+                </div>
+                <div className="flex-item-right-las">
+                        :
+                </div>
+
+
+                
+            </div>
+                
+            <button className="green-btn" >Scheduled</button>
+            <hr class="rounded"/>
+            <div className="sub-items">
+                <div className="flex-item-left">
+                      Valid Through
+                      <div className="sub-items-small">
+                      <div className="flex-item-left">
+
+                      17/07/2021-18/07/2021
+                      </div>
+                </div>
+                </div>
+                <div className="flex-item-right">
+                        Valid on
+                        <div className="sub-items-small">
+                         <div className="flex-item-left">
+
+                            All Week
+                        </div>
+                      </div>
+                </div>
+
+
+                
+            </div>
+
+            <div className="sub-items">
+                <div className="flex-item-left">
+                        Type
+                      <div className="sub-items-small">
+                      <div className="flex-item-left">
+
+                      17/07/2021-18/07/2021
+                      </div>
+                </div>
+                </div>
+                <div className="flex-item-right">
+                         <div className="image-stack">
+                        <div className="image-a">
+                            <img src={Img}alt="foodImage"></img>
+                        </div>
+                        <div className="image-a" style={{left:"15px"}}>
+                            <img src={MoMoImg}alt="foodImage"></img>
+                        </div>
+                        <div className="image-a"  style={{left:"30px"}}>
+                            <img src={FoodImg}alt="foodImage" ></img>
+                        </div>
+                        
+                        
+                    </div>
+                </div>                
+            </div>
+              
+                
+            </div>
+                
+
+                
+
+
+
+
+              </div>
+        
+
+        
+
+
+         </div>
+
+   
+
+    
+
+
+
+</div>
+  
+  
   );
 }
 
